c47a4a0f14aa236d6dd552bd24850486cd3d2350
Preston-Wong <preston.wong25@ycdsbk12.ca>
2025-05-12 16:51:47 -0400

    finished tier 2 problem 8

--- src/recursion/Tier_2/SumOfDigits.java
+++ src/recursion/Tier_2/SumOfDigits.java
@@ -2,4 +2,26 @@ package recursion.Tier_2;
 
 public class SumOfDigits {
     
+    public static int sumOfDigits(int n) {
+
+        // base case
+        if (n == 0) {
+
+            return 0;
+
+        // recurssive case 
+        } else {
+
+            return (n % 10) + sumOfDigits(n/10);
+
+        }
+
+    }
+
+    public static void main(String args[]) {
+
+        System.out.println(sumOfDigits(1234)); // output 10
+
+    }
+
 }
