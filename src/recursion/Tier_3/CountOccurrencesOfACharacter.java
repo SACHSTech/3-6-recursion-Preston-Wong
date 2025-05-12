fac27007a178ac389d1c51289737b0f30dc8e15b
Preston-Wong <preston.wong25@ycdsbk12.ca>
2025-05-12 17:29:41 -0400

    finished tier 3 problem 11

--- src/recursion/Tier_3/CountOccurrencesOfACharacter.java
+++ src/recursion/Tier_3/CountOccurrencesOfACharacter.java
@@ -1,5 +1,43 @@
 package recursion.Tier_3;
 
+/**
+ * counts the number of times a specific character appears in a string 
+ */
 public class CountOccurrencesOfACharacter {
     
+    /**
+     * counts the number of times that a specific characterr appears in a string 
+     * 
+     * @param s string that is being scanned 
+     * @param c character that we are checking for 
+     * @return the number of times that character appears in the string 
+     */
+    public static int countChar(String s, char c) {
+
+        int index = 0;
+
+        // base case
+        if (index == s.length()) {
+
+            return 0;
+
+        // recurssive case
+        } else if (s.charAt(index) == c) {
+
+            return countChar(s.substring(1), c) + 1;
+
+        } else {
+
+            return countChar(s.substring(1), c);
+
+        }
+        
+    }
+
+    public static void main(String args[]) {
+
+        System.out.println(countChar("hello", 'l')); // output 2
+
+    }
+
 }
