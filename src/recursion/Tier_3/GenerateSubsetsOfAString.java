d979051b3782fd01705419b1bf95865d1acd0013
Preston-Wong <preston.wong25@ycdsbk12.ca>
2025-05-12 17:40:01 -0400

    added comments and classes for tier 3 problem 12, no logic code WIP

--- src/recursion/Tier_3/GenerateSubsetsOfAString.java
+++ src/recursion/Tier_3/GenerateSubsetsOfAString.java
@@ -1,5 +1,26 @@
 package recursion.Tier_3;
 
+/**
+ * generates substrings of the inputted string 
+ */
 public class GenerateSubsetsOfAString {
     
+    /**
+     * generate substrings of the inputtedd string  
+     * 
+     * @param s String that we are generating substrings from
+     * @return substrings generated 
+     */
+    public static String generateSubStrings(String s) {
+
+       return null;
+
+    }
+
+    public static void main (String args[]) {
+
+        System.out.println(generateSubStrings("abc")); // output "", "a", "b", "c", "ab", "ac", "bc", "abc"
+
+    }
+
 }
