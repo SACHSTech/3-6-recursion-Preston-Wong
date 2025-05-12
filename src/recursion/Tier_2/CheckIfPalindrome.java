444df9dc8c809e1a8c71917ab15b4b8eb5d1617a
Preston-Wong <preston.wong25@ycdsbk12.ca>
2025-05-12 15:55:22 -0400

    finished tier 2 problem 2

--- src/recursion/Tier_2/CheckIfPalindrome.java
+++ src/recursion/Tier_2/CheckIfPalindrome.java
@@ -2,21 +2,34 @@ package recursion.Tier_2;
 
 public class CheckIfPalindrome {
     
-    public static String CheckIfPalindrome(String text) {
+    public static boolean iSPalindrome(String text) {
 
         int index = text.length();
 
-        if (index != 0) {
+        // base case
+        if (index == 0 || index == 1) {
 
+            return true;
 
+        } else if (text.charAt(0) != text.charAt(index - 1)) {
 
+            return false;
+
+        // recursive case
+        } else {
+
+            return iSPalindrome(text.substring(1, index - 1));
         }
 
     }
 
     public static void main(String[] args) {
-        
 
+        System.out.println(iSPalindrome("madam")); // Output: true
+        System.out.println(iSPalindrome("hello")); // Output: false
+        System.out.println(iSPalindrome("racecar")); // Output: true
+        System.out.println(iSPalindrome("level")); // Output: true
+        System.out.println(iSPalindrome("world")); // Output: false
 
     }
 
