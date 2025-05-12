438ef6adf7061435879cdf0e20693ff872a562cd
Preston-Wong <preston.wong25@ycdsbk12.ca>
2025-05-12 17:12:59 -0400

    finished tier 3 problem 10

--- src/recursion/Tier_3/FindMaximumInAnArray.java
+++ src/recursion/Tier_3/FindMaximumInAnArray.java
@@ -1,5 +1,39 @@
 package recursion.Tier_3;
 
+/**
+ * finds the maximum number in the array to a certain index 
+ */
 public class FindMaximumInAnArray {
     
+    /**
+     * finds the largest number in the array until a certain index 
+     * 
+     * @param arr array of integers
+     * @param n the index value the user wants to stop chekcing at
+     * @return the largest integer value 
+     */
+    public static int findMax(int[] arr, int n) {
+
+        // base case
+        if (n == 0) {
+
+            return 0;
+
+        // recurssive case 
+        } else {
+
+            return Math.max(arr[n - 1], findMax(arr, n - 1));
+
+        }
+
+    }
+
+    public static void main(String args[]) {
+
+        int[] numbers = {1,2,3,4,5,6,7,8,9};
+
+        System.out.println(findMax(numbers, 5)); // output 5
+
+    }
+
 }
