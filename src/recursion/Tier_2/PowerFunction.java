83e0636b957a54178f60c476386dde09b38f33be
Preston-Wong <preston.wong25@ycdsbk12.ca>
2025-05-12 15:58:26 -0400

    finished tier 2, problem 3

--- src/recursion/Tier_2/PowerFunction.java
+++ src/recursion/Tier_2/PowerFunction.java
@@ -2,4 +2,27 @@ package recursion.Tier_2;
 
 public class PowerFunction {
     
+    public static int power(int base, int exponenet) {
+
+        // base case
+        if (exponenet == 0) {
+
+            return 1;
+
+        // recurssive case
+        } else {
+
+            return base * power(base, exponenet -1);
+
+        }
+
+    }
+
+    public static void main(String args[]) {
+
+        System.out.println(power(2,3)); // output 8
+        System.out.println(power(2,5)); // output 32
+
+    }
+
 }
