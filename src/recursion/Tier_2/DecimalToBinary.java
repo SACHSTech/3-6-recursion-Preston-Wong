6baccb7f747539a763ccc64b09af5377563354d4
Preston-Wong <preston.wong25@ycdsbk12.ca>
2025-05-12 16:29:23 -0400

    finished tier 2 problem 7

--- src/recursion/Tier_2/DecimalToBinary.java
+++ src/recursion/Tier_2/DecimalToBinary.java
@@ -2,4 +2,27 @@ package recursion.Tier_2;
 
 public class DecimalToBinary {
     
+    public static int toBinary(int n) {
+
+
+        // base case
+        if (n == 0) {
+
+            return 0;
+
+        // recurssive case
+        } else {
+
+            return ((n % 2) + 10 * toBinary(n / 2));
+
+        }
+
+    }
+
+    public static void main(String args[]) {
+
+        System.out.println(toBinary(10)); // output 1010
+
+    }
+
 }
